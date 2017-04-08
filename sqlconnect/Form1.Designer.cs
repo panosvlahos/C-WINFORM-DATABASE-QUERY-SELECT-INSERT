87e9b9d4a4a6b5dad56e21e58de2066f9e0280c3
panosvlahos <panos.vlahos@hotmail.com>
2017-04-08 19:50:59 +0300

    sql

---
+++ sqlconnect/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace sqlconnect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(830, 158);
+            this.dataGridView2.TabIndex = 0;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(197, 207);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(79, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(328, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ψάξε την ταινία";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 207);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(158, 245);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 29);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Πρόσθεσε μία νέα ταινία";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(36, 280);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(176, 280);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(328, 280);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(478, 280);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 8;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(628, 280);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 9;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(339, 245);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(139, 29);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Πρόσθεσε την ταινία";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(741, 191);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(101, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Διέγραψε την ταινία";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(868, 328);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView2);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+      
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ sqlconnect/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace sqlconnect
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "Βάλε εδώ το Id της ταινίας";
+            button1.Text = "Ψάξε την ταινία";
+            button2.Text = "Πρόσθεσε μία νέα ταινία";
+            button3.Text = "Πρόσθεσε την ταινία";
+            button4.Text = "Διέγραψε την ταινία";
+            textBox2.Enabled = false;
+            textBox3.Enabled = false;
+            textBox4.Enabled = false;
+            textBox5.Enabled = false;
+            textBox6.Enabled = false;
+            button3.Enabled = false;
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string strConnectionString = @"Data Source=PANOS-PC\SQLEXPRESS;Initial Catalog=MovieDB;Integrated Security=True";
+
+            SqlConnection cn = new SqlConnection(strConnectionString);
+            string textboxValue = textBox1.Text;
+            if (String.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("Bάλτε μία τιμή παρακαλώ.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                //dataGridView1.Rows.Clear();
+            }
+            try
+            {
+                cn.Open();
+                string Query = "SELECT * FROM AllMovies WHERE Id='" + textboxValue + "'; "; 
+                SqlCommand DisplayTableData = new SqlCommand(Query, cn);
+                SqlDataAdapter sda = new SqlDataAdapter();
+                sda.SelectCommand = DisplayTableData;
+                DataTable dbdataset = new DataTable();
+                sda.Fill(dbdataset);
+                BindingSource bSource = new BindingSource();
+
+                bSource.DataSource = dbdataset;
+                dataGridView2.DataSource = bSource;
+                sda.Update(dbdataset);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Enabled = false;
+            textBox2.Enabled = true;
+            textBox3.Enabled = true;
+            textBox4.Enabled = true;
+            textBox5.Enabled = true;
+            textBox6.Enabled = true;
+            button3.Enabled = true;
+            button1.Enabled = false;
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string strConnectionString = @"Data Source=PANOS-PC\SQLEXPRESS;Initial Catalog=MovieDB;Integrated Security=True";
+
+            SqlConnection cn = new SqlConnection(strConnectionString);
+            string textboxValue2 = textBox2.Text;
+            string textboxValue3 = textBox3.Text;
+            string textboxValue4 = textBox4.Text;
+            string textboxValue5 = textBox5.Text;
+            string textboxValue6 = textBox6.Text;
+
+            try
+            {
+
+                SqlCommand cmd = new SqlCommand("SET IDENTITY_INSERT dbo.AllMovies ON;INSERT INTO AllMovies (Id, Title, Genre, ReleaseYear, Director ) VALUES (@Id, @Title, @Genre, @ReleaseYear, @Director)SET IDENTITY_INSERT dbo.AllMovies OFF");
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = cn;
+                cmd.Parameters.AddWithValue("@Id", textboxValue2);
+                cmd.Parameters.AddWithValue("@Title", textboxValue3);
+                cmd.Parameters.AddWithValue("@Genre", textboxValue4);
+                cmd.Parameters.AddWithValue("@ReleaseYear", textboxValue5);
+                cmd.Parameters.AddWithValue("@Director", textboxValue6);
+                cn.Open();
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (dataGridView2.SelectedRows.Count == 0)
+            {
+
+                MessageBox.Show("No row selected !");// show a message here to inform
+            }
+
+           if(dataGridView2.SelectedRows.Count ==1)
+            {
+                MessageBox.Show("selected");
+            }
+
+            string strConnectionString = @"Data Source=PANOS-PC\SQLEXPRESS;Initial Catalog=MovieDB;Integrated Security=True";
+
+            string sql = "DELETE FROM  dbo.AllMovies WHERE Id = @rowID";
+            SqlConnection cn = new SqlConnection(strConnectionString);
+            SqlCommand deleteRecord = new SqlCommand(sql, cn);
+                // this overcomes  the out of bound error message 
+                // if the selectedRow is greater than 0 then exectute the code below.
+            if (dataGridView2.CurrentCell.RowIndex > 0)
+                {
+
+                    int selectedIndex = dataGridView2.SelectedRows[0].Index;
+                    // gets the RowID from the first column in the grid
+                    int rowID = Convert.ToInt32(dataGridView2[0, selectedIndex].Value);
+
+                    // Add the parameter to the command collection
+                    deleteRecord.Parameters.Add("@rowID", SqlDbType.Int).Value = rowID;
+                
+
+                // Remove the row from the grid
+                dataGridView2.Rows.RemoveAt(selectedIndex);
+                }
+
+            }
+
+        
+        private void label1_Click(object sender, EventArgs e)
+        {     
+        }
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        { 
+        }
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+       
+    }
+
+       
+    }
+
